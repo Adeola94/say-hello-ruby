5fb28f4e48f7c6ea441fb654f9dd99f7fc306908
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-12-11 22:19:40 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
-def say_hello (name = "Adeola", language = "Ruby")
+def say_hello = (name = "Adeola", language = "Ruby")
   puts "Hi, my name is #{name}, I am learning #{language}"
 end 
 
-say_hello
+say_hello 
